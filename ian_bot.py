cd1fab3b8b69204a62d30d7b2d1951ba795bf9e1
Daniel Florea <dflorea@uc.cl>
2023-03-16 01:44:50 -0300

    Testing the new features on deploy 🕵️🔧

--- ian_bot.py
+++ ian_bot.py
@@ -64,7 +64,7 @@ def check_json_data():
 scheduler = BackgroundScheduler(timezone='America/Santiago')
 
 # Schedule the check_json_data function to run every day at 10 AM
-trigger = CronTrigger(hour=1, minute=1, timezone='America/Santiago')
+trigger = CronTrigger(hour=1, minute=50, timezone='America/Santiago')
 scheduler.add_job(check_json_data, trigger = trigger)
 
 # Start the scheduler
